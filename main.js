02de7740b8b1244c59e36723994c8ce0df4d37cf
JSaavedra <jsaavedra@live.cl>
2023-07-30 23:40:09 -0400

    se agrega evento click a las cartas para ejecutar funcion flipCard que hace girar las cartas

---
+++ main.js
@@ -0,0 +1,8 @@
+const cards = document.querySelectorAll(".card")
+
+cards.forEach((card) => card.addEventListener("click", flipCard))
+
+function flipCard() {
+    this.classList.add("flipped")
+}
+
