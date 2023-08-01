55160ec72006457add17ecbb2ae118b12b6ffd69
JSaavedra <jsaavedra@live.cl>
2023-08-01 13:10:30 -0400

    se agrega data card value en el html y se crea la variable flippedCards

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const cards = document.querySelectorAll(".card")
+let flippedCards = [];
 
 cards.forEach((card) => card.addEventListener("click", flipCard))
+console.log(cards)
 
 function flipCard() {
     this.classList.add("flipped")
