465af702cbc5baef772b54b31923b3861cf0628f
JSaavedra <jsaavedra@live.cl>
2023-08-02 00:07:16 -0400

    se agrega funcion checkformatch

--- main.js
+++ main.js
@@ -2,9 +2,43 @@ const cards = document.querySelectorAll(".card")
 let flippedCards = [];
 
 cards.forEach((card) => card.addEventListener("click", flipCard))
-console.log(cards)
 
 function flipCard() {
-    this.classList.add("flipped")
+
+this.classList.add("flipped")
+    flippedCards.push(this);
+
+    checkForMatch(
+        flippedCards[0].dataset.cardValue,
+        flippedCards[1].dataset.cardValue,
+        
+    );
+
 }
 
+function checkForMatch(cardValue1, cardValue2) {
+
+    
+    let card1 = flippedCards[0];
+    let card2 = flippedCards[1];
+    if (cardValue1 === cardValue2) {
+      // Las cartas son iguales, las dejamos mostradas
+
+        
+        flippedCards = [];
+    
+
+    } else {
+
+      // Las cartas no son iguales, las volvemos a ocultar
+        setTimeout(() => {
+        
+        card1.classList.remove("flipped");
+        card2.classList.remove("flipped");
+        flippedCards = [];
+        
+
+    }, 
+    1500);
+}
+}
